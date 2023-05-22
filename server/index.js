1488f8de0d9b77353b2dcfb7d10be682a98b7ec6
DoDi1408 <ro.chavez.5454@gmail.com>
2023-05-21 21:26:56 -0600

    Fixed typos

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json' assert { type: "json" };;
+import swaggerDocument from './swagger.json' assert { type: "json" };
 
 //declaring routes
 import asignacionRoutes from './routes/asignacion.js';
@@ -14,20 +14,20 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cors())
+app.use(cors());
 
 //swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //routes
 app.use('/asignacion', asignacionRoutes);
-app.use('/comentario', comenatrioRoutes)
-app.use('/solicitud', solcitudRoutes)
-app.use('/usario', usuarioRoutes)
+app.use('/comentario', comenatrioRoutes);
+app.use('/solicitud', solcitudRoutes);
+app.use('/usario', usuarioRoutes);
 
 
 
 //MongoDB Cloud Atlas
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));

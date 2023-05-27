9031f77a83db18bb79285a0e9c9af7cda6ca9f61
unknown <bernasconivargas.mateo@gmail.com>
2023-05-27 12:30:35 -0600

    added navbar starter

--- client/GestionImaginantes-Front/src/App.jsx
+++ client/GestionImaginantes-Front/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import SideBar from './components/SideBar/SideBar';
+import TopBar from './components/TopBar/TopBar';
 import Inicio from './pages/Inicio';
 import Progreso from './pages/Progreso';
 import Solicitudes from './pages/Solicitudes';
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className='mainContainer'>
      <SideBar />
+     <TopBar progress={50} name={'Rodrigo Chavez'} level={'Nivel 50'}/>
      <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/progreso" element={<Progreso />} />
---
+++ client/GestionImaginantes-Front/src/components/TopBar/TopBar.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { AppBar, Toolbar, Typography, Box, LinearProgress } from "@mui/material";
+import './styles.css'
+
+
+const TopBar = ({progress, name, level}) => {
+  return (
+      <AppBar position="static" sx={{ bgcolor: "#FFFFFF" }} className='rounding'>
+        <Toolbar sx={{justifyContent:"space-between"}}>
+          <div className='progress-container'>
+            <Typography className='progreso-text'>
+              PROGRESO
+            </Typography>
+            <LinearProgress className='progress-bar' variant='determinate' value={progress}/>
+          </div>
+          <Typography className='progreso-text'>
+            Viernes, 28 de abril 2023
+          </Typography>
+          <Box className="profile-container">
+          <Typography noWrap={true} className=''>{name}</Typography>
+          <Typography noWrap={true} className=''>{level}</Typography>
+        </Box>
+        </Toolbar>
+      </AppBar>
+  );
+}
+
+export default TopBar;

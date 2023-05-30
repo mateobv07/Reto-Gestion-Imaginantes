e4a0b1cbc57afa2c73b5e3d14dd4219e4093362b
unknown <bernasconivargas.mateo@gmail.com>
2023-05-30 10:56:05 -0600

    added upcoming assignemnt endpoint

--- server/controllers/assignment.js
+++ server/controllers/assignment.js
@@ -1,9 +1,21 @@
+import { db } from '../index.js'
+
 export const getAssignments = async (req, res) => {
     res.status(200);
 }
 
-export const getAssignmentsPending = async (req, res) => {
-    res.status(200);
+export const getUpcomingAssignment = async (req, res) => {
+    const { studentID } = req.params;
+    db('Assignment')
+        .where('studentID',studentID)
+        .andWhere('taskStatus', 'En espera de confirmacion')
+        .orderBy('dueDate', 'asc')
+        .first()
+        .then(assignment => {
+            console.log(assignment);
+            res.status(200).json(assignment);
+        })
+        .catch(err => res.status(400).json('Unable to get upcoming task'));
 }
 
 export const updateAssignment = async (req, res) => {

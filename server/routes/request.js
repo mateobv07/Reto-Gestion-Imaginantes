b34a74f5be2d9364af879669197ac2be1b76766d
unknown <bernasconivargas.mateo@gmail.com>
2023-06-12 18:35:20 -0600

    added endpoint functionality

--- server/routes/request.js
+++ server/routes/request.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getOtherRequests, createRequest, updateRequest, deleteRequest, getMyRequests } from "../controllers/request.js"
+import { getOtherRequests, createRequest, updateRequest, deleteRequest, getMyRequests, getOne } from "../controllers/request.js"
 import Auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get('/:studentID', Auth, getOtherRequests);
-router.get('/:studentID/my', Auth, getMyRequests);
+router.get('/', Auth, getOtherRequests);
+router.get('/my', Auth, getMyRequests);
+router.get('/:id', Auth, getOne);
 router.post('/', Auth, createRequest);
 router.patch('/:id', Auth, updateRequest);
 router.delete('/:id', Auth, deleteRequest);
